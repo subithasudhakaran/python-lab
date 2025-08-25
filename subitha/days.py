8a5a45409344495d6b6317d460eb3d124883ff4b
Subitha Sudhakaran <145781414+subithasudhakaran@users.noreply.github.com>
2025-08-25 16:21:09 +0530

    Add files via upload

---
+++ subitha/days.py
@@ -0,0 +1,16 @@
+number=int(input("enter the number"))
+if number==1:
+    print("monday")
+elif number==2:
+    print("tuesday")
+elif number==3:
+    print("wenesday")
+elif number==4:
+    print("thursday")
+elif number==5:
+    print("friday")
+elif number==6:
+    print("saturday")
+elif number==7:
+    print("sunday")
+    
---
+++ subitha/positiveornegative.py
@@ -0,0 +1,8 @@
+a=int(input("enter a number"))
+if a>0:
+    print("a is a positive number")
+elif a==0:
+    print("a is zero")
+else:
+    print(" a  is negative number")
+    
