3c9c857a3798ad8b5486b65ce9baa06e9694a446
Subitha Sudhakaran <145781414+subithasudhakaran@users.noreply.github.com>
2025-11-03 16:01:50 +0530

    Add files via upload

---
+++ graphics/circle.py
@@ -0,0 +1,6 @@
+def circarea(r):
+    area=3.14*r*r
+    print("Area of circle is",area)
+def circperimeter(r):
+        perimeter=2*3.14*r
+        print("Perimeter of circle is",perimeter)
---
+++ graphics/rectangle.py
@@ -0,0 +1,6 @@
+def arearect(l,b):
+    area=l*b
+    print("Area of rectangle is",area)
+def perimeterrect(l,b):   
+    perimeter=2*(l+b)
+    print("perimeter of rectangle is",perimeter)
